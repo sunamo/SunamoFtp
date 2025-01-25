386b3be032295b09907a4562b9eeebcab55dc06d
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-25 19:33:32 +0100

    25.1.25.1 Removed absolute paths exists in my PC filesystem

--- Other/FtpTest.cs
+++ Other/FtpTest.cs
@@ -17,24 +17,5 @@ public class FtpTest
         ftpBase.setRemotePass(pw);
     }
 
-    public static void FtpDll(string un, string pw)
-    {
-        var ftpDll = new FtpDllWrapper(new Ftp());
-        SetConnectionInfo(ftpDll, un, pw);
-        var ftp = ftpDll.Client;
-
-        ftp.Connect(ftpDll.remoteHost);
-        ftp.Login(ftpDll.remoteUser, ftpDll.remotePass);
-
-        var folder = "a";
-        ftpDll.DebugActualFolder();
-        ftpDll.DebugAllEntries();
-
-        ftp.CreateFolder("/" + folder);
-        ftp.ChangeFolder(folder);
-        ftpDll.DebugActualFolder();
-        ftp.UploadFiles("D:\a.txt");
-
-        ftp.Close();
-    }
+    
 }
