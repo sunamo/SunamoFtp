9747baea85a261745ebc37608e48db4f7c72f007
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:09:11 +0100

    Remove another unused methods

--- _sunamo/SunamoExceptions/ThrowEx.cs
+++ _sunamo/SunamoExceptions/ThrowEx.cs
@@ -2,8 +2,6 @@ namespace SunamoFtp._sunamo.SunamoExceptions;
 internal partial class ThrowEx
 {
 
-    internal static bool IsNullOrEmpty(string argName, string argValue)
-    { return ThrowIsNotNull(Exceptions.IsNullOrWhitespace(FullNameOfExecutedCode(), argName, argValue, true)); }
     internal static bool NotImplementedMethod() { return ThrowIsNotNull(Exceptions.NotImplementedMethod); }
 
     internal static bool NotSupported() { return ThrowIsNotNull(Exceptions.NotSupported(FullNameOfExecutedCode())); }
